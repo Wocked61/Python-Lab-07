6620e0f8877676101808a41d985866791bd3a624
Wocked61 <DylanP@csu.fullerton.edu>
2024-10-26 00:26:34 -0700

    "completed the lab07 contacts list with classes"

--- contacts.py
+++ contacts.py
@@ -4,20 +4,38 @@ class Contacts:
     def __init__(self, filename = 'contacts.json'):
         self.filename = filename
         self.data = {}
+        try:
+            with open(self.filename, 'r') as file:
+                self.data = json.load(file)
+        except:
+            pass
     
     def add_contact(self, id, first_name, last_name):
         if (id in self.data):
             return 'error'
-        id:[first_name , last_name]
+        self.data[id] = [first_name , last_name]
         #sort
+        self.data = dict(sorted(self.data.items(), key= lambda item: (item[1][1].lower(), item[1][0].lower())))
         #write contents
-        return([first_name, last_name])
+        with open(self.filename, 'w') as file:
+            json.dump(self.data, file)
+        return{id:[first_name,last_name]}
 
-    def modify_contacts(self, id, first_name, last_name):
-        if (id == 'dne'):
+    def modify_contact(self, id, first_name, last_name):
+        if id not in self.data:
             return 'error'
-        self.data.append(id:[first_name, last_name])
+        self.data[id] = [first_name,last_name]
+        self.data = dict(sorted(self.data.items(), key= lambda item: (item[1][1].lower(), item[1][0].lower())))
+        with open(self.filename, 'w') as file:
+            json.dump(self.data, file)
+        return{id:[first_name,last_name]}
 
-    def delete_contact(self, id)
-        if (id =='dne'):
-            return 0
+    def delete_contact(self, id):
+        if (id not in self.data):
+            return 'error'
+        delete = {id:self.data.pop(id)}
+        with open(self.filename, 'w') as file:
+            json.dump(self.data, file)
+            return delete
+
+        
--- main.py
+++ main.py
@@ -1,26 +1,56 @@
-import contacts
+from contacts import Contacts
+
+contacts = Contacts()
+
+filename = 'contacts.json'
 
 while True:
     inp1 = input('''
       *** TUFFY TITAN CONTACT MAIN MENU
 
-     1. Add contact
-     2. Modify contact
-     3. Delete contact
-     4. Print contact list
-     5. Set contact filename
-     6. Exit the program
+1. Add contact
+2. Modify contact
+3. Delete contact
+4. Print contact list
+5. Set contact filename
+6. Exit the program
 
 Enter menu choice: ''')
     if (inp1 == '1') :
-        
+        Phone = input('Enter phone number: ')
+        First_name = input('Enter first name: ')
+        Last_name = input('Enter last name: ')
+        add = contacts.add_contact(id=Phone, first_name=First_name, last_name=Last_name)
+        if (add == 'error'):
+            print('Phone number already exists.\n')
+        else:
+            print(f'Added: {First_name} {Last_name}.\n')
     if (inp1 == '2') :
-
+        Phone = input('Enter phone number: ')
+        First_name = input('Enter first name: ')
+        Last_name = input('Enter last name: ')
+        modify = contacts.modify_contact(id=Phone, first_name=First_name, last_name=Last_name)
+        if modify == 'error':
+            print('Phone number does not exist.\n')
+        else:
+            print(f'Modified: {First_name} {Last_name}.')
     if (inp1 == '3'):
+        Phone = input('Enter phone number: ')
+        delete = contacts.delete_contact(id=Phone)
+        if delete == 'error':
+            print('Invalid phone number.')
+        else:
+            print(f'Deleted: {delete[Phone][0]} {delete[Phone][1]}.')
 
     if (inp1 == '4') :
-
+        print('''
+==================== CONTACT LIST ====================
+Last Name             First Name            Phone
+====================  ====================  ==========''')
+        for id, (first_name, last_name) in contacts.data.items():
+            print(f"{last_name:22} {first_name:20} {id}")
+    
     if (inp1 == '5') :
-
+        filename = input('Enter File Name: ')
     if (inp1 == '6') :
         break
